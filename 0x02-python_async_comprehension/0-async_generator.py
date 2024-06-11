560ad9a936d335a4b8f47713e84e1f728fce7182
AbduGad <abdo79gad@gmail.com>
2024-06-12 00:25:47 +0300

    "Added three Python scripts: 0-async_generator.py, 1-async_comprehension.py, and 2-measure_runtime.py, with implementations of an async generator, async comprehension, and runtime measurement using asyncio.gather."

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,1 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" Coroutine with async """
+
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[float, None, None]:
+    """ Loops 10 times asyncronously, yields random num """
+
+    for i in range(10):
+        yield random.random()
+        await asyncio.sleep(1)
